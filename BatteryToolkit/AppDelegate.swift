4398e21d5f8cd9f7ed86f3c98d50785ce556a983
Marvin Häuser <mhaeuser@posteo.de>
2022-09-04 10:25:45 +0200

    Revamp GUI

--- BatteryToolkit/AppDelegate.swift
+++ BatteryToolkit/AppDelegate.swift
@@ -1,93 +1,24 @@
 import Cocoa
 
-extension NSTextField {
-
-    /// Return an `NSTextField` configured exactly like one created by dragging a “Label” into a storyboard.
-    class func newLabel(stringValue: String) -> NSTextField {
-        let label = NSTextField()
-        label.isEditable = false
-        label.isSelectable = false
-        label.textColor = .labelColor
-        label.backgroundColor = .controlColor
-        label.drawsBackground = false
-        label.isBezeled = false
-        label.alignment = .natural
-        label.font = NSFont.systemFont(ofSize: NSFont.systemFontSize(for: label.controlSize))
-        label.lineBreakMode = .byClipping
-        label.cell?.isScrollable = true
-        label.cell?.wraps = false
-        label.stringValue = stringValue
-        return label
-    }
-
-    /// Return an `NSTextField` configured exactly like one created by dragging a “Wrapping Label” into a storyboard.
-    class func newWrappingLabel(stringValue: String) -> NSTextField {
-        let label = newLabel(stringValue: stringValue)
-        label.lineBreakMode = .byWordWrapping
-        label.cell?.isScrollable = false
-        label.cell?.wraps = true
-        return label
-    }
-
-}
-
 @main
-class AppDelegate: NSObject, NSApplicationDelegate {
-
-    // We need to declare NSStatusItem here, otherwise it gets destroyed after
-    // applicationDidFinishLaunching is called
-    var statusBarItem : NSStatusItem!
-    var statusBarMenu : NSMenu!
+final class AppDelegate: NSObject, NSApplicationDelegate {
+    @IBOutlet weak var statusBarMenu: NSMenu!
+    private var statusBarItem : NSStatusItem!
     
-    var textView: NSTextView!
-    var textStore: NSTextStorage!
+    @IBOutlet weak var powerAdapterMenuItem: NSMenuItem!
+    @IBOutlet weak var chargingMenuItem: NSMenuItem!
     
-    var started: Bool = false
+    private var started: Bool = false
 
     func applicationDidFinishLaunching(_ aNotification: Notification) {
-        // Returns the system-wide status bar located in the menu bar.
-        let statusBar = NSStatusBar.system
-
-        // Returns a newly created status item that has been allotted a specified space within the status bar.
-        self.statusBarItem = statusBar.statusItem(withLength: NSStatusItem.squareLength)
-        self.statusBarItem.button?.image = NSImage(systemSymbolName: "star.fill", accessibilityDescription: "Status bar icon")
-
-        // An object that manages an app’s menus.
-        self.statusBarMenu = NSMenu()
-        
-        //let title = NSMenuItem()
-        //title.attributedTitle = NSAttributedString(string: "Battery Toolkit", attributes: [.font: NSFont.boldSystemFont(ofSize: 0.0)])
-        //self.statusBarMenu.addItem(title)
-
-        self.statusBarMenu.addItem(withTitle: "External Power: Enabled", action: nil, keyEquivalent: "")
-        self.statusBarMenu.addItem(withTitle: "Charging On Hold", action: nil, keyEquivalent: "")
-        
-        self.statusBarMenu.addItem(NSMenuItem.separator())
-        
-        let disablePower = NSMenuItem(title: "Disable External Power", action: #selector(AppDelegate.disableExternalPower), keyEquivalent: "d")
-        disablePower.target = AppDelegate.self
-        self.statusBarMenu.addItem(disablePower)
-        let enablePower = NSMenuItem(title: "Enable External Power", action: #selector(AppDelegate.enableExternalPower), keyEquivalent: "D")
-        enablePower.target = AppDelegate.self
-        enablePower.isAlternate = true
-        self.statusBarMenu.addItem(enablePower)
-        
-        let chargeFull = NSMenuItem(title: "Charge to Full Now", action: #selector(AppDelegate.chargeToFull), keyEquivalent: "c")
-        chargeFull.target = AppDelegate.self
-        self.statusBarMenu.addItem(chargeFull)
-        let chargeMax = NSMenuItem(title: "Charge to Maximum Now", action: #selector(AppDelegate.chargeToMaximum), keyEquivalent: "C")
-        chargeMax.target = AppDelegate.self
-        chargeMax.isAlternate = true
-        self.statusBarMenu.addItem(chargeMax)
-        
-        self.statusBarMenu.addItem(NSMenuItem.separator())
-        
-        self.statusBarMenu.addItem(NSMenuItem(title: "Quit", action: #selector(NSApplication.terminate(_:)), keyEquivalent: "q"))
-
-        // Add menu to statusbar
-        self.statusBarItem.menu = self.statusBarMenu
-        
         started = BatteryToolkit.start()
+        // FIXME: Handle error
+
+        self.statusBarItem = NSStatusBar.system.statusItem(
+            withLength: NSStatusItem.squareLength
+            )
+        self.statusBarItem.button?.image = NSImage(named: NSImage.Name("StatusItemIcon"))
+        self.statusBarItem.menu = self.statusBarMenu
     }
     
     func applicationWillTerminate(_ aNotification: Notification) {
@@ -97,23 +28,19 @@ class AppDelegate: NSObject, NSApplicationDelegate {
         }
     }
 
-    func applicationSupportsSecureRestorableState(_ app: NSApplication) -> Bool {
-        return true
-    }
-
-    @objc private static func disableExternalPower() {
-        BatteryToolkit.disableExternalPower()
+    @IBAction private func disablePowerAdapter(sender: NSMenuItem) {
+        BatteryToolkit.disablePowerAdapter()
     }
     
-    @objc private static func enableExternalPower() {
-        BatteryToolkit.enableExternalPower()
+    @IBAction private func enablePowerAdapter(sender: NSMenuItem) {
+        BatteryToolkit.enablePowerAdapter()
     }
 
-    @objc private static func chargeToMaximum() {
+    @IBAction private func chargeToMaximum(sender: NSMenuItem) {
         BatteryToolkit.chargeToMaximum()
     }
 
-    @objc private static func chargeToFull() {
+    @IBAction private func chargeToFull(sender: NSMenuItem) {
         BatteryToolkit.chargeToFull()
     }
 }
---
+++ BatteryToolkit/PreferencesViewController.swift
@@ -0,0 +1,113 @@
+import Cocoa
+
+final class PreferencesViewController: NSViewController {
+    @IBOutlet weak var minChargeTextField: NSTextField!
+    @IBOutlet weak var minChargeSlider: NSSlider!
+    
+    @IBOutlet weak var maxChargeTextField: NSTextField!
+    @IBOutlet weak var maxChargeSlider: NSSlider!
+    
+    @IBOutlet weak var adapterSleepButton: NSButton!
+
+    private var minChargeVal = BTPreferencesInfo.minChargeDefault
+    @objc dynamic var minChargeNum: NSNumber {
+        get {
+            return NSNumber(value: self.minChargeVal)
+        }
+        
+        set {
+            let value = newValue.intValue
+            //
+            // For clamping, the assignment needs to be async, because otherwise
+            // the source control does not get notified of the update. We cannot
+            // change the values of the UI controls directly, because this
+            // caues the NSSlider to sometimes visually desync with its value.
+            //
+            if value < BTPreferencesInfo.minChargeMin {
+                DispatchQueue.main.async {
+                    self.minChargeNum = NSNumber(
+                        value: BTPreferencesInfo.minChargeMin
+                        )
+                }
+                
+            } else if value > 100 {
+                DispatchQueue.main.async {
+                    self.minChargeNum = NSNumber(value: 100)
+                }
+            } else {
+                self.minChargeVal = UInt8(value)
+                //
+                // Clamp the maximum charge to be at least the minimum charge.
+                //
+                if self.maxChargeVal < self.minChargeVal {
+                    self.maxChargeNum = self.minChargeNum
+                }
+            }
+        }
+    }
+    
+    private var maxChargeVal = BTPreferencesInfo.maxChargeDefault
+    @objc dynamic var maxChargeNum: NSNumber {
+        get {
+            return NSNumber(value: self.maxChargeVal)
+        }
+        
+        set {
+            let value = newValue.intValue
+            //
+            // See minChargeNum for an explanation.
+            //
+            if value < BTPreferencesInfo.maxChargeMin {
+                DispatchQueue.main.async {
+                    self.maxChargeNum = NSNumber(
+                        value: BTPreferencesInfo.maxChargeMin
+                        )
+                }
+            } else if value > 100 {
+                DispatchQueue.main.async {
+                    self.maxChargeNum = NSNumber(value: 100)
+                }
+            } else {
+                self.maxChargeVal = UInt8(value)
+                //
+                // Clamp the maximum charge to be at least the minimum charge.
+                //
+                if self.maxChargeVal < self.minChargeVal {
+                    self.minChargeNum = self.maxChargeNum
+                }
+            }
+        }
+    }
+    
+    private func initAdapterSleepState() {
+        self.adapterSleepButton.state = BTPreferencesInfo.adapterSleep ?
+            NSControl.StateValue.off :
+            NSControl.StateValue.on
+    }
+    
+    @IBAction func restoreDefaultsButtonAction(_ sender: NSButton) {
+        self.minChargeNum = NSNumber(value: BTPreferencesInfo.minChargeDefault)
+        self.maxChargeNum = NSNumber(value: BTPreferencesInfo.maxChargeDefault)
+        self.initAdapterSleepState()
+    }
+    
+    @IBAction func cancelButtonAction(_ sender: NSButton) {
+        self.view.window?.windowController?.close()
+    }
+    
+    @IBAction func okButtonAction(_ sender: NSButton) {
+        BTHelperXPCClient.setChargeLimits(
+            minCharge: self.minChargeVal,
+            maxCharge: self.maxChargeVal
+            )
+        BTHelperXPCClient.setAdapterSleep(
+            enabled: self.adapterSleepButton.state == NSControl.StateValue.off
+            )
+        self.view.window?.windowController?.close()
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.initAdapterSleepState()
+    }
+}
