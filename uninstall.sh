70c92e802a4fa2db20f93a644469ab7c64350e70
Marvin Häuser <mhaeuser@posteo.de>
2022-10-01 20:37:50 +0200

    Document uninstall script

--- uninstall.sh
+++ uninstall.sh
@@ -5,13 +5,17 @@
 # SPDX-License-Identifier: BSD-3-Clause
 ##
 
+# Remove the Battery Toolkit daemon.
 sudo rm /Library/LaunchDaemons/me.mhaeuser.batterytoolkitd.plist
 sudo rm /Library/PrivilegedHelperTools/me.mhaeuser.batterytoolkitd
 sudo launchctl remove me.mhaeuser.batterytoolkitd
 
+# Remove the Battery Toolkit daemon data.
 sudo defaults delete me.mhaeuser.batterytoolkitd
 sudo security authorizationdb remove me.mhaeuser.batterytoolkitd.manage
 
+# Remove the Battery Toolkit Autostart helper.
 launchctl remove me.mhaeuser.BatteryToolkitAutostart
 
+# Remove the Battery Toolkit app data.
 defaults remove me.mhaeuser.BatteryToolkit
