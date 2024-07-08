mhaeuser/Battery-Toolkit

commit 3d2394ea589bc1c931d6c70b455be5d26d46975b

Support syncing the MagSafe indicator light
Addresses https://github.com/mhaeuser/Battery-Toolkit/issues/13

me.mhaeuser.batterytoolkitd/BTPowerEvents.swift
@@ -67,9 +67,14 @@ internal enum BTPowerEvents {
         return self.enableBelowThresholdMode(threshold: BTSettings.maxCharge)
     }
 
-    static func disableCharging() -> Bool {
+    static func disableCharging(percent: UInt8) -> Bool {
         self.chargingMode = .standard
-        return BTPowerState.disableCharging()
+        return BTPowerState.disableCharging(percent: percent)
+    }
+
+    static func disableCharging() -> Bool {
+        let (percent, _, _) = BTPowerState.getPercentRemaining()
+        return self.disableCharging(percent: percent)
     }
 
     static func chargeToFull() -> Bool {
@@ -155,12 +160,12 @@ internal enum BTPowerEvents {
         switch self.chargingMode {
         case .toMaximum:
             if percent < BTSettings.maxCharge {
-                _ = BTPowerState.enableCharging()
+                _ = BTPowerState.enableCharging(percent: percent)
             }
 
         case .toFull:
             if percent < 100 {
-                _ = BTPowerState.enableCharging()
+                _ = BTPowerState.enableCharging(percent: percent)
             }
 
         case .standard:
@@ -200,10 +205,10 @@ internal enum BTPowerEvents {
                 //
                 // Charging modes are reset once we disable charging.
                 //
-                _ = BTPowerEvents.disableCharging()
+                _ = BTPowerEvents.disableCharging(percent: percent)
             }
         } else if percent < BTSettings.minCharge {
-            _ = BTPowerState.enableCharging()
+            _ = BTPowerState.enableCharging(percent: percent)
         }
 
         return percent
@@ -253,9 +258,13 @@ internal enum BTPowerEvents {
         // of development machines.
         //
         #if !DEBUG
-            _ = BTPowerState.enableCharging()
+            let (percent, _, _) = self.getPercentRemaining()
+            _ = BTPowerState.enableCharging(percent: percent)
             _ = BTPowerState.enablePowerAdapter()
         #endif
+        if BTSettings.magSafeSync {
+            _ = SMCComm.MagSafe.setSystem()
+        }
     }
 
     private static func enableBelowThresholdMode(threshold: UInt8) -> Bool {
@@ -274,7 +283,7 @@ internal enum BTPowerEvents {
         }
 
         if percent < threshold {
-            return BTPowerState.enableCharging()
+            return BTPowerState.enableCharging(percent: percent)
         }
 
         return true

Battery Toolkit.xcodeproj/project.pbxproj
@@ -63,6 +63,7 @@
 		CE7F623528B95A8200EF58F9 /* BTPreprocessor.m in Sources */ = {isa = PBXBuildFile; fileRef = CEE387BD28A836AA00C2007C /* BTPreprocessor.m */; };
 		CE85D05F28D7B7E6005D1E0C /* BTDaemonManagement+Service.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE85D05E28D7B7E6005D1E0C /* BTDaemonManagement+Service.swift */; };
 		CE8ED7F428C144EE00398581 /* BTDispatcher.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE8ED7F328C144EE00398581 /* BTDispatcher.swift */; };
+		CE9E6DEC2BF106A10070ABB2 /* SMCComm+MagSafe.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE9E6DEB2BF106A10070ABB2 /* SMCComm+MagSafe.swift */; };
 		CEA3909328DFAE1C0023261A /* BTXPCValidation.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE50557028D65B48000E4387 /* BTXPCValidation.swift */; };
 		CEA80FCB285A27CF0064ADEA /* SMCComm.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE37DD0D283F86F90060C43F /* SMCComm.swift */; };
 		CEA80FCC285A27D20064ADEA /* SMCComm+Power.swift in Sources */ = {isa = PBXBuildFile; fileRef = CE37DD0F284164160060C43F /* SMCComm+Power.swift */; };
@@ -211,6 +212,7 @@
 		CE85D05E28D7B7E6005D1E0C /* BTDaemonManagement+Service.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "BTDaemonManagement+Service.swift"; sourceTree = "<group>"; };
 		CE8ED7F328C144EE00398581 /* BTDispatcher.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BTDispatcher.swift; sourceTree = "<group>"; };
 		CE9BD24628E8908000B6C61D /* AutostartHelper.app */ = {isa = PBXFileReference; explicitFileType = wrapper.application; includeInIndex = 0; path = AutostartHelper.app; sourceTree = BUILT_PRODUCTS_DIR; };
+		CE9E6DEB2BF106A10070ABB2 /* SMCComm+MagSafe.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = "SMCComm+MagSafe.swift"; sourceTree = "<group>"; };
 		CEA80FD3285A51600064ADEA /* NSXPCConnection+AuditToken.h */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.c.h; path = "NSXPCConnection+AuditToken.h"; sourceTree = "<group>"; };
 		CEA80FD5285A565C0064ADEA /* BTDaemonComm.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BTDaemonComm.swift; sourceTree = "<group>"; };
 		CEA80FD7285A56B50064ADEA /* BTDaemon.swift */ = {isa = PBXFileReference; lastKnownFileType = sourcecode.swift; path = BTDaemon.swift; sourceTree = "<group>"; };
@@ -363,6 +365,7 @@
 				CE37DD0D283F86F90060C43F /* SMCComm.swift */,
 				CE37DD0F284164160060C43F /* SMCComm+Power.swift */,
 				CEE4A6ED2B9B855100300506 /* IOPSPrivate.swift */,
+				CE9E6DEB2BF106A10070ABB2 /* SMCComm+MagSafe.swift */,
 			);
 			path = Libraries;
 			sourceTree = "<group>";
@@ -673,6 +676,7 @@
 				CE50557128D65B48000E4387 /* BTXPCValidation.swift in Sources */,
 				CE2B96C028A7E2BB00FE3DDB /* GlobalSleep.swift in Sources */,
 				CE3179EC28DB543200E9C121 /* SimpleAuth.swift in Sources */,
+				CE9E6DEC2BF106A10070ABB2 /* SMCComm+MagSafe.swift in Sources */,
 				CEA80FD0285A27FB0064ADEA /* BTPowerEvents.swift in Sources */,
 				CE3C1DA828CA33FD008A0F7D /* BTLegacyHelperInfo.swift in Sources */,
 				CE8ED7F428C144EE00398581 /* BTDispatcher.swift in Sources */,

BatteryToolkit/Views/Settings/BTSettingsViewController.swift
@@ -25,6 +25,7 @@ internal final class BTSettingsViewController: NSViewController {
     @IBOutlet private var maxChargeSlider: NSSlider!
 
     @IBOutlet private var adapterSleepSwitch: NSSwitch!
+    @IBOutlet private var magSafeSyncSwitch: NSSwitch!
 
     private var minChargeVal = BTSettingsInfo.Defaults.minCharge
     @objc private dynamic var minChargeNum: NSNumber {
@@ -123,6 +124,9 @@ internal final class BTSettingsViewController: NSViewController {
             BTSettingsInfo.Keys.adapterSleep: NSNumber(
                 value: self.adapterSleepSwitch.state == .off
             ),
+            BTSettingsInfo.Keys.magSafeSync: NSNumber(
+                value: self.magSafeSyncSwitch.state == .on
+            ),
         ]
         //
         // Submit the settings to the daemon only when they changed.
@@ -198,6 +202,10 @@ internal final class BTSettingsViewController: NSViewController {
         self.adapterSleepSwitch.state = value ? .off : .on
     }
 
+    private func setMagSafeSync(value: Bool) {
+        self.magSafeSyncSwitch.state = value ? .on : .off
+    }
+
     private func initGeneralState() {
         let autostart = UserDefaults.standard.bool(
             forKey: self.autostartSetting
@@ -235,6 +243,13 @@ internal final class BTSettingsViewController: NSViewController {
                 self.setMinCharge(value: minCharge)
                 self.setMaxCharge(value: maxCharge)
                 self.setAdapterSleep(value: adapterSleep)
+
+                if let magSafeSync = magSafeSyncNum?.boolValue {
+                    self.magSafeSyncSwitch.isEnabled = true
+                    self.setMagSafeSync(value: magSafeSync)
+                } else {
+                    self.magSafeSyncSwitch.isEnabled = false
+                }
             }
         }
     }

Common/BTSettingsInfo.swift
@@ -1,5 +1,5 @@
 //
-// Copyright (C) 2022 Marvin Häuser. All rights reserved.
+// Copyright (C) 2022 - 2024 Marvin Häuser. All rights reserved.
 // SPDX-License-Identifier: BSD-3-Clause
 //
 
@@ -10,6 +10,7 @@ internal enum BTSettingsInfo {
         static let minCharge: UInt8 = 70
         static let maxCharge: UInt8 = 80
         static let adapterSleep = false
+        static let magSafeSync = false
     }
 
     enum Bounds {
@@ -21,6 +22,7 @@ internal enum BTSettingsInfo {
         static let minCharge = "MinCharge"
         static let maxCharge = "MaxCharge"
         static let adapterSleep = "AdapterSleep"
+        static let magSafeSync = "MagSafeSync"
     }
 
     static func chargeLimitsValid(

Libraries/SMCComm+MagSafe.swift
@@ -1,0 +1,80 @@
+//
+// Copyright (C) 2024 Marvin Häuser. All rights reserved.
+// SPDX-License-Identifier: BSD-3-Clause
+//
+
+public extension SMCComm {
+    @MainActor
+    enum MagSafe {
+        private(set) static var supported = false
+
+        static func prepare() {
+            //
+            // Ensure all required SMC keys are present and well-formed.
+            //
+            for keyInfo in self.keys {
+                let info = SMCComm.getKeyInfo(key: keyInfo.key)
+                guard keyInfo.info == info else {
+                    self.supported = false
+                    return
+                }
+            }
+
+            self.supported = true
+        }
+
+        static func setSystem() -> Bool {
+            return self.setColor(color: 0x00)
+        }
+
+        static func setOff() -> Bool {
+            return self.setColor(color: 0x01)
+        }
+
+        static func setGreen() -> Bool {
+            return self.setColor(color: 0x03)
+        }
+
+        static func setOrange() -> Bool {
+            return self.setColor(color: 0x04)
+        }
+
+        static func setOrangeSlowBlink() -> Bool {
+            return self.setColor(color: 0x06)
+        }
+
+        static func setOrangeFastBlink() -> Bool {
+            return self.setColor(color: 0x07)
+        }
+
+        static func setOrangeBlinkOff() -> Bool {
+            return self.setColor(color: 0x19)
+        }
+    }
+}
+
+private extension SMCComm.MagSafe {
+    private enum Keys {
+        static let ACLC = SMCComm.Key("A", "C", "L", "C")
+    }
+
+    private static let keys =
+        [
+            SMCComm.KeyInfo(
+                key: SMCComm.MagSafe.Keys.ACLC,
+                info: SMCComm.KeyInfoData(
+                    dataSize: 1,
+                    dataType: SMCComm.KeyTypes.ui8,
+                    dataAttributes: 0xD4
+                )
+            ),
+        ]
+
+    private static func setColor(color: UInt8) -> Bool {
+        guard self.supported else {
+            return false
+        }
+
+        return SMCComm.writeKeyUI8(key: self.Keys.ACLC, value: color)
+    }
+}

me.mhaeuser.batterytoolkitd/BTPowerState.swift
@@ -29,6 +29,16 @@ internal enum BTPowerState {
             //
             self.disableAdapterSleep()
         }
+
+        SMCComm.MagSafe.prepare()
+
+        if BTSettings.magSafeSync {
+            self.syncMagSafeState()
+        }
+    }
+
+    static func getPercentRemaining() -> (UInt8, Bool, Bool) {
+        return IOPSPrivate.GetPercentRemaining() ?? (100, false, false)
     }
 
     static func adapterSleepSettingToggled() {
@@ -46,7 +56,39 @@ internal enum BTPowerState {
         }
     }
 
-    static func disableCharging() -> Bool {
+    static func syncMagSafeStatePowerEnabled(percent: UInt8) {
+        assert(BTSettings.magSafeSync)
+        assert(!self.powerDisabled)
+
+        if percent == 100 {
+            _ = SMCComm.MagSafe.setGreen()
+        } else if self.chargingDisabled {
+            _ = SMCComm.MagSafe.setOrange()
+        } else {
+            _ = SMCComm.MagSafe.setOrangeSlowBlink()
+        }
+    }
+
+    static func syncMagSafeState() {
+        assert(BTSettings.magSafeSync)
+
+        if self.powerDisabled {
+            _ = SMCComm.MagSafe.setOff()
+        } else {
+            let (percent, _, _) = self.getPercentRemaining()
+            self.syncMagSafeStatePowerEnabled(percent: percent)
+        }
+    }
+
+    static func magSafeSyncSettingToggled() {
+        if BTSettings.magSafeSync {
+            self.syncMagSafeState()
+        } else {
+            _ = SMCComm.MagSafe.setSystem()
+        }
+    }
+
+    static func disableCharging(percent: UInt8) -> Bool {
         guard !self.chargingDisabled else {
             return true
         }
@@ -57,13 +99,18 @@ internal enum BTPowerState {
             return false
         }
 
+        self.chargingDisabled = true
+
+        if BTSettings.magSafeSync {
+            BTPowerState.syncMagSafeStatePowerEnabled(percent: percent)
+        }
+
         GlobalSleep.restore()
 
-        self.chargingDisabled = true
         return true
     }
 
-    static func enableCharging() -> Bool {
+    static func enableCharging(percent: UInt8) -> Bool {
         guard self.chargingDisabled else {
             return true
         }
@@ -77,6 +124,11 @@ internal enum BTPowerState {
         GlobalSleep.disable()
 
         self.chargingDisabled = false
+
+        if BTSettings.magSafeSync {
+            BTPowerState.syncMagSafeStatePowerEnabled(percent: percent)
+        }
+
         return true
     }
 
@@ -94,6 +146,10 @@ internal enum BTPowerState {
             return false
         }
 
+        if BTSettings.magSafeSync {
+            _ = SMCComm.MagSafe.setOff()
+        }
+
         self.powerDisabled = true
         return true
     }
@@ -109,9 +165,15 @@ internal enum BTPowerState {
             return false
         }
 
+        self.powerDisabled = false
+
+        if BTSettings.magSafeSync {
+            let (percent, _, _) = self.getPercentRemaining()
+            _ = BTPowerState.syncMagSafeStatePowerEnabled(percent: percent)
+        }
+
         self.restoreAdapterSleep()
 
-        self.powerDisabled = false
         return true
     }
 

me.mhaeuser.batterytoolkitd/BTSettings.swift
@@ -11,10 +11,14 @@ internal enum BTSettings {
     private(set) static var minCharge = BTSettingsInfo.Defaults.minCharge
     private(set) static var maxCharge = BTSettingsInfo.Defaults.maxCharge
     private(set) static var adapterSleep = BTSettingsInfo.Defaults.adapterSleep
+    private(set) static var magSafeSync = BTSettingsInfo.Defaults.magSafeSync
 
     static func readDefaults() {
         self.adapterSleep = UserDefaults.standard.bool(
             forKey: BTSettingsInfo.Keys.adapterSleep
+        )
+        self.magSafeSync = UserDefaults.standard.bool(
+            forKey: BTSettingsInfo.Keys.magSafeSync
         )
 
         let minCharge = UserDefaults.standard.integer(
@@ -43,6 +47,9 @@ internal enum BTSettings {
             forKey: BTSettingsInfo.Keys.adapterSleep
         )
         UserDefaults.standard.removeObject(
+            forKey: BTSettingsInfo.Keys.magSafeSync
+        )
+        UserDefaults.standard.removeObject(
             forKey: BTSettingsInfo.Keys.minCharge
         )
         UserDefaults.standard.removeObject(
@@ -56,11 +63,17 @@ internal enum BTSettings {
         let minCharge = NSNumber(value: self.minCharge)
         let maxCharge = NSNumber(value: self.maxCharge)
         let adapterSleep = NSNumber(value: self.adapterSleep)
-        let settings: [String: NSObject & Sendable] = [
+        let magSafeSync = NSNumber(value: self.magSafeSync)
+        var settings: [String: NSObject & Sendable] = [
             BTSettingsInfo.Keys.minCharge: minCharge,
             BTSettingsInfo.Keys.maxCharge: maxCharge,
             BTSettingsInfo.Keys.adapterSleep: adapterSleep,
         ]
+
+        if SMCComm.MagSafe.supported {
+            settings.updateValue(magSafeSync,
+                forKey: BTSettingsInfo.Keys.magSafeSync)
+        }
 
         return settings
     }
@@ -92,6 +105,13 @@ internal enum BTSettings {
             BTSettingsInfo.Defaults.adapterSleep
 
         self.setAdapterSleep(enabled: adapterSleep)
+
+        let magSafeSyncNum =
+            settings[BTSettingsInfo.Keys.magSafeSync] as? NSNumber
+        let magSafeSync = magSafeSyncNum?.boolValue ??
+            BTSettingsInfo.Defaults.magSafeSync
+
+        self.setMagSafeSync(enabled: magSafeSync)
 
         self.writeDefaults()
 
@@ -130,6 +150,16 @@ internal enum BTSettings {
         BTPowerState.adapterSleepSettingToggled()
     }
 
+    private static func setMagSafeSync(enabled: Bool) {
+        guard self.magSafeSync != enabled else {
+            return
+        }
+
+        self.magSafeSync = enabled
+
+        BTPowerState.magSafeSyncSettingToggled()
+    }
+
     private static func writeDefaults() {
         assert(
             BTSettingsInfo.chargeLimitsValid(
@@ -149,6 +179,10 @@ internal enum BTSettings {
         UserDefaults.standard.set(
             self.adapterSleep,
             forKey: BTSettingsInfo.Keys.adapterSleep
+        )
+        UserDefaults.standard.set(
+            self.magSafeSync,
+            forKey: BTSettingsInfo.Keys.magSafeSync
         )
         //
         // As NSUserDefaults are not automatically synchronized without

commit c2dfd19064015984d118c0d85eede3818adc3bdb

Fix Release build
Issue introduced by 3d2394ea589bc1c931d6c70b455be5d26d46975b.

me.mhaeuser.batterytoolkitd/BTPowerEvents.swift
@@ -258,7 +258,7 @@ internal enum BTPowerEvents {
         // of development machines.
         //
         #if !DEBUG
-            let (percent, _, _) = self.getPercentRemaining()
+            let (percent, _, _) = BTPowerState.getPercentRemaining()
             _ = BTPowerState.enableCharging(percent: percent)
             _ = BTPowerState.enablePowerAdapter()
         #endif