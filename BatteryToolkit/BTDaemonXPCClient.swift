d7d6f74f13ab96557acf36b83fe7f2474eab7a0b
Marvin Häuser <8659494+mhaeuser@users.noreply.github.com>
2024-10-05 18:55:20 +0200

    Don't prompt for permission for basic commands

--- BatteryToolkit/BTDaemonXPCClient.swift
+++ BatteryToolkit/BTDaemonXPCClient.swift
@@ -57,33 +57,30 @@ internal enum BTDaemonXPCClient {
     }
 
     static func enablePowerAdapter() async throws {
-        let authData = try await BTAppXPCClient.getManageAuthorization()
         try await withCheckedThrowingContinuation { continuation in
             self.runExecute(
                 continuation: continuation,
-                authData: authData,
+                authData: nil,
                 command: BTDaemonCommCommand.enablePowerAdapter
             )
         }
     }
 
     static func chargeToThreshold() async throws {
-        let authData = try await BTAppXPCClient.getManageAuthorization()
         try await withCheckedThrowingContinuation { continuation in
             self.runExecute(
                 continuation: continuation,
-                authData: authData,
+                authData: nil,
                 command: BTDaemonCommCommand.chargeToThreshold
             )
         }
     }
 
     static func chargeToFull() async throws {
-        let authData = try await BTAppXPCClient.getManageAuthorization()
         try await withCheckedThrowingContinuation { continuation in
             self.runExecute(
                 continuation: continuation,
-                authData: authData,
+                authData: nil,
                 command: BTDaemonCommCommand.chargeToFull
             )
         }
